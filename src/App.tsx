00fce2633a0907dbd95e4d501754fbb066e4de2d
Alson Garbuja <magar33alson@gmail.com>
2024-09-28 22:22:05 +0545

    WIP: accessing bookmarks and listing them

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const getBookMarks = async () => {
+      const bookmarks = await chrome.bookmarks.getTree();
+      console.log("Bookmarks");
+      console.log(bookmarks);
+    };
+
+    getBookMarks();
+  }, []);
+
+  const addBookMark = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.bookmarks.create({
+        parentId: "1",
+        title: tabs[0].title,
+        url: tabs[0].url,
+      });
+    });
+  };
+
   return (
     <>
-      <h1>Bookmark killer</h1>
+      <h1>UnMark</h1>
+      <button onClick={addBookMark}>Add Bookmark</button>
     </>
   );
 }
