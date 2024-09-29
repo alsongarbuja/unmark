206d49df91de8f0239fda7c3f940376e96e879a8
Alson Garbuja <magar33alson@gmail.com>
2024-09-29 23:19:45 +0545

    WIP: bookmarks list fetch and sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,47 @@
-import { useEffect } from "react";
-import "./App.css";
+import { useCallback, useEffect, useState } from "react";
+import moment from "moment";
 
 function App() {
-  useEffect(() => {
-    const getBookMarks = async () => {
-      const bookmarks = await chrome.bookmarks.getTree();
-      console.log("Bookmarks");
-      console.log(bookmarks);
-    };
+  const [bookmarks, setBookmarks] = useState<IBookmark[]>([]);
+  const [sortBy, setSortBy] = useState<"dateAdded" | "lastUsed">("dateAdded");
 
+  const sortBookmarks = useCallback(() => {
+    setBookmarks((prev) =>
+      prev.sort((a, b) => {
+        if (sortBy === "dateAdded") {
+          return b.dateAdded - a.dateAdded;
+        } else {
+          return b.dateLastUsed - a.dateLastUsed;
+        }
+      })
+    );
+  }, [sortBy]);
+
+  const extractBookmark = useCallback(
+    (bookmarks: IBookmarkGroup[] | IBookmark[]) => {
+      bookmarks.forEach((bookmark) => {
+        if ("children" in bookmark) {
+          extractBookmark(bookmark.children);
+        } else {
+          setBookmarks((prev) => [...prev, bookmark as IBookmark]);
+        }
+      });
+      sortBookmarks();
+    },
+    [sortBookmarks]
+  );
+
+  const getBookMarks = useCallback(async () => {
+    const bookmarks = await chrome.bookmarks.getTree();
+    if (bookmarks.length === 0) {
+      return;
+    }
+    extractBookmark(bookmarks as unknown as IBookmarkGroup[]);
+  }, [extractBookmark]);
+
+  useEffect(() => {
     getBookMarks();
-  }, []);
+  }, [getBookMarks]);
 
   const addBookMark = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -23,10 +54,36 @@ function App() {
   };
 
   return (
-    <>
+    <main className="bg-slate-900 text-white py-2 px-4 min-w-80">
       <h1>UnMark</h1>
+      <div className="flex flex-col items-start gap-1">
+        <label htmlFor="sortby">Sort By</label>
+        <select
+          value={sortBy}
+          onChange={(e) =>
+            setSortBy(e.target.value as "dateAdded" | "lastUsed")
+          }
+          className="border rounded-md p-1 w-full text-black"
+        >
+          <option value="dateAdded">Date Added</option>
+          <option value="lastUsed">Last Used</option>
+        </select>
+      </div>
+      <ul className="mt-4">
+        {bookmarks.map((bookmark) => (
+          <li
+            key={bookmark.id}
+            className="flex items-center justify-between px-1 py-2 my-2 border rounded-md"
+          >
+            <a href={bookmark.url} target="_blank" rel="noreferrer">
+              {bookmark.title}
+            </a>
+            ({moment(bookmark.dateLastUsed).fromNow()})
+          </li>
+        ))}
+      </ul>
       <button onClick={addBookMark}>Add Bookmark</button>
-    </>
+    </main>
   );
 }
 
---
+++ src/types/index.d.ts
@@ -0,0 +1,19 @@
+interface IBookmarkGroup {
+  id: string;
+  title: string;
+  parentId: string;
+  dateAdded: number;
+  dateLastUsed: number;
+  index: number;
+  children: IBookmarkGroup[] | IBookmark[];
+}
+
+interface IBookmark {
+  id: string;
+  title: string;
+  url: string;
+  parentId: string;
+  dateAdded: number;
+  dateLastUsed: number;
+  index: number;
+}
