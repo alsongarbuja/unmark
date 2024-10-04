16c84840bf3ddd5fefc2eb52355c8fca387db457
Alson Garbuja <magar33alson@gmail.com>
2024-10-04 22:35:20 +0545

    background: add service worker file for alarm and notification API

---
+++ public/alarm.js
@@ -0,0 +1,32 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension Installed");
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  const [, title] = alarm.name.split("*_*");
+  chrome.notifications.create(alarm.name, {
+    title: "Unmark - Reminder",
+    message: `Time to visit ( ${title} ) bookmark`,
+    type: "basic",
+    iconUrl: "/icons/unmark-icon-128x128.png",
+    buttons: [
+      {
+        title: "Open Bookmark",
+      },
+      { title: "Snooze 5 minutes" },
+    ],
+  });
+});
+
+chrome.notifications.onButtonClicked.addListener(
+  (notificationId, buttonIndex) => {
+    const [url, title] = notificationId.split("*_*");
+    if (buttonIndex === 0) {
+      chrome.tabs.create({ url });
+    } else {
+      chrome.alarms.create(`${url}*_*${title}`, {
+        when: Date.now() + 5 * 1000 * 60,
+      });
+    }
+  }
+);
